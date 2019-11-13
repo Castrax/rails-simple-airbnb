4dfdd020d7e9ca30eacd9405f4e5288b2bde388d
Mathilde Robert <m.robert@skema.edu>
2019-11-13 22:23:25 +0100

    flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+5.times do
+  flats = Flat.new(name: Faker::Company.catch_phrase, address: Faker::Address.street_address, description: Faker::Restaurant.description, price_per_night: Faker::Number.number(digits: 3), number_of_guests: Faker::Number.number(digits: 2))
+  flats.save!
+end
+
+puts "Finished!"
